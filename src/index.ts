5b563cbf1478184035a9c412835aed84e6602fce
vorabhol chuaybhan <vorabhol.chuaybhan@ibm.com>
2022-08-01 08:20:02 +0700

    init source

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+ 
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+
+		const body = await request.json()
+		await env.pm25.put(`pm25-${(new Date()).getTime()}.json`, JSON.stringify(body), {
+			httpMetadata: request.headers,
+
+		});
+
+		console.log(JSON.stringify( body))
+		return new Response("Hello World! tf1");
+	},
+
+
+};
